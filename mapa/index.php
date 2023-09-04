a1d099fdc7a49c8dd0d7f0af735050bbcc78d817
santiagoma7 <129330790+santiagoma7@users.noreply.github.com>
2023-09-04 15:29:03 -0500

    actualizar mapa index.php

--- mapa/index.php
+++ mapa/index.php
@@ -404,19 +404,19 @@ if (isset($_GET['cerrar_sesion'])) {
                     <h3>Reserva</h3>
                     <br>
                     <p>La principal razón de nuestro proyecto es brindar a los aprendices la facilidad de reservar un locker, para que asi se reduzca el tiempo que gasta en buscar un casillero disponible que en muchos casos esta busqueda es en vano.</p>
-                    <button>Reserva ya</button>
+                    <button><a href="#hero">Reserva ya</button></a>
                 </div>
                 <div class="carta">
                     <h3>Administración</h3>
                     <br>
                     <p>Hemos creado cenilockers no solo para hacer la vida de los aprendices un poco mas sencilla,si no para que los administradores de nuestra sede tengan a la mano el estado de los casilleros, asi mismo se tenga en cuenta información de la persona que tiene guardas sus pertenencias y asi tener una correcta administración de quien ocupa un casillero para poder solucionar problemas</p>
-                    <button>¿Alguna duda?</button>
+                    <button><a href="../Soporte/index.php">Reportes</button></a>
                 </div>
                 <div class="carta">
                     <h3>Reporte</h3>
                     <br>
                     <p>Estamos comprometidos con el binestar y el sentido de pertenencia por nuestra institución es por esto que tanto aprendices y administradores pueden hacer el reporte de los casilleros averiados, permitiendonos saber el estado de nuestros lockers .</p>
-                    <button><a href="../reportes/paginas/index.php">Reportes</button></a>
+                    <button><a href="../reportes/index.php">Reportes</button></a>
                 </div>  
             </div>
         </div>
@@ -435,7 +435,7 @@ if (isset($_GET['cerrar_sesion'])) {
 
     <section id="final">
         <h2>¿Listo para reservar?</h2>
-        <button>RESERVA YA!</button>
+        <button><a href="#hero">RESERVA YA!</a></button>
     </section>
 
     <footer>
